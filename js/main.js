1d675e102535c522fa916656ff78b524d826794a
web-py-g <61905059+web-py-g@users.noreply.github.com>
2020-09-27 17:02:10 +0300

    Refresh site

---
+++ js/main.js
@@ -0,0 +1,96 @@
+(function () {
+	const header = document.querySelector('.header');
+	window.onscroll = () => {
+		if(window.pageYOffset > 50){
+			header.classList.add('header_active')
+		}else{
+			header.classList.remove('header_active')
+		}
+	}
+}());
+
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("dot");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	}	
+
+
+(function () {
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
+	const menuLink = document.querySelectorAll('.header__link')
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header__nav_active')
+	});
+	menuCloseItem.addEventListener('click', () => {
+		menu.classList.remove('header__nav_active')
+	});
+	if (window.innerWidth <= 670){
+		for (let i = 0; i < menuLink.length; i+=1){
+			menuLink[i].addEventListener('click', () => {
+				menu.classList.remove('header__nav_active');
+			})
+		}
+	}
+}());
+
+
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
